6e8040f0d6e070766f9089206f9646557e01b115
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-11-14 00:07:25 -0600

    Cors added

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const bodyParser = require('body-parser/index');
 const app = express();
+const cors = require('cors')
 const { getEncuesta, postEncuesta } = require('./Metodos');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.route('/api/informacion').get(getEncuesta).post(postEncuesta);
 
